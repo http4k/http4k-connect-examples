d80c34a99777cbb976196f04a3977bad6248d324
David Denton <denton.david@gmail.com>
2022-07-02 15:01:18 +0100

    upgrade all versions and APIs for examples

--- extending-connect-adapters/src/main/kotlin/actions/GetRepo.kt
+++ extending-connect-adapters/src/main/kotlin/actions/GetRepo.kt
@@ -1,13 +1,23 @@
 package actions
 
-import org.http4k.connect.github.action.GitHubAction
-import org.http4k.connect.kClass
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Success
+import org.http4k.connect.RemoteFailure
+import org.http4k.connect.github.api.action.GitHubAction
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.format.Moshi
 import java.time.Instant
 
-data class GetRepo(val owner: String, val repo: String) : GitHubAction<OrgRepo>(kClass()) {
+data class GetRepo(val owner: String, val repo: String) : GitHubAction<OrgRepo> {
     override fun toRequest() = Request(GET, "/repos/$owner/$repo")
+    override fun toResult(response: Response) = with(response) {
+        when {
+            status.successful -> Success(Moshi.asA<OrgRepo>(bodyString()))
+            else -> Failure(RemoteFailure(toRequest().method, toRequest().uri, status, bodyString()))
+        }
+    }
 }
 
 data class OrgRepo(val name: String, val created_at: Instant)
--- extending-connect-adapters/src/test/kotlin/actions/GetRepoTest.kt
+++ extending-connect-adapters/src/test/kotlin/actions/GetRepoTest.kt
@@ -3,9 +3,9 @@ package actions
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import dev.forkhandles.result4k.Success
-import org.http4k.cloudnative.env.Secret
-import org.http4k.connect.github.GitHub
-import org.http4k.connect.github.Http
+import org.http4k.connect.github.GitHubToken
+import org.http4k.connect.github.api.GitHub
+import org.http4k.connect.github.api.Http
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
@@ -17,7 +17,7 @@ class GetRepoTest {
     private val input = OrgRepo("http4k", EPOCH)
     private val app = { _: Request -> Response(OK).body(asFormatString(input)) }
 
-    private val gitHub = GitHub.Http({ Secret("hello") }, app)
+    private val gitHub = GitHub.Http({ GitHubToken.of("hello") }, app)
 
     @Test
     fun `get repo`() {
